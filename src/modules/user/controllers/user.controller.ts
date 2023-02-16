3ccb912ecb437a809636c52390131a6892724931
PanasheT <ptafuma@proton.me>
2023-02-16 14:55:27 +0200

    feat: add create user endpoint

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from '../dtos';
+import { UserDto, UserDtoFactory } from '../dtos/user.dto';
 import { UserService } from '../services';
 
 @Controller('users')
 @ApiTags('users')
 export class UserController {
-    constructor(private readonly service: UserService) {}
+  constructor(private readonly service: UserService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new user.' })
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({
+    description: 'The user has been successfully created.',
+    type: UserDto,
+  })
+  public async createNewUser(@Body() model: CreateUserDto): Promise<UserDto> {
+    const user = await this.service.createUser(model);
+    return UserDtoFactory(user);
+  }
 }
