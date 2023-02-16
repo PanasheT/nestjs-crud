ffaad3cc15cdb43c35dc051f2ffe364dcaeeaab9
PanasheT <ptafuma@proton.me>
2023-02-16 14:08:57 +0200

    feat: add create user dto

--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+}
