f5fefe8eeb67f8cc45d1c0d09db121ccd704d6a4
PanasheT <ptafuma@proton.me>
2023-02-16 14:22:10 +0200

    feat: add create user

--- src/modules/user/factories/user.factory.ts
+++ src/modules/user/factories/user.factory.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos';
 import { UserEntity } from '../entities';
 
 @Injectable()
@@ -8,4 +9,28 @@ export class UserFactory {
   constructor(
     @InjectRepository(UserEntity) private readonly repo: Repository<UserEntity>
   ) {}
+
+  public async createUser(model: CreateUserDto): Promise<UserEntity> {
+    await this.assertUserExists(model.email, model.username);
+
+    const user: UserEntity = Object.assign(new UserEntity(), { ...model });
+
+    return user;
+  }
+
+  private async assertUserExists(
+    email: string,
+    username: string
+  ): Promise<void> {
+    const isUser: boolean = Boolean(
+      await this.repo.findOneBy([
+        { email, deleted: false },
+        { username, deleted: false },
+      ])
+    );
+
+    if (isUser) {
+      throw new BadRequestException('User already exists.');
+    }
+  }
 }
