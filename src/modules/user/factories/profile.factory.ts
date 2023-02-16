f5fd5317a4c87c92801ddf91ac08303e1e115360
PanasheT <ptafuma@proton.me>
2023-02-16 19:26:43 +0200

    feat: add update profile to factory

--- src/modules/user/factories/profile.factory.ts
+++ src/modules/user/factories/profile.factory.ts
@@ -1,8 +1,24 @@
-import { CreateProfileDto } from '../dtos';
+import { BadRequestException } from '@nestjs/common';
+import { UtilityService as util } from 'src/util';
+import { CreateProfileDto, UpdateProfileDto } from '../dtos';
 import { ProfileEntity } from '../entities';
 
 export class ProfileFactory {
   public createProfile(model: CreateProfileDto): ProfileEntity {
     return Object.assign(new ProfileEntity(), model);
+  }
+
+  public updateProfile(
+    model: UpdateProfileDto,
+    profile: ProfileEntity
+  ): ProfileEntity {
+    const update: Partial<ProfileEntity> = util.validateUpdate(profile, model);
+
+    if (Object.entries(update).length === 0) {
+      throw new BadRequestException('Update has no changes.');
+    }
+
+    Object.assign(profile, update);
+    return profile;
   }
 }
