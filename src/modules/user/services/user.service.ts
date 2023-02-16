0f36495f4a86f9ee2c388c877b77b7fe52682156
PanasheT <ptafuma@proton.me>
2023-02-16 14:24:23 +0200

    feat: add user types

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../entities';
+import { UserFactory } from '../factories';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserEntity)
-        private readonly repo: Repository<UserEntity>,    
-    ) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly repo: Repository<UserEntity>,
+    private readonly factory: UserFactory
+  ) {}
 }
---
+++ src/modules/user/types/user.types.ts
@@ -0,0 +1,1 @@
+export type UserIdentificationProperties = 'username' | 'email' | 'uuid';
