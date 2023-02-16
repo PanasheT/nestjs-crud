910d0109cb5aafda87add40f8abf13d2e8fec65b
PanasheT <ptafuma@proton.me>
2023-02-16 14:29:43 +0200

    feat: add find user query type

--- src/modules/user/types/user.types.ts
+++ src/modules/user/types/user.types.ts
@@ -1,1 +1,6 @@
+import { FindQuery } from 'src/common';
+import { UserEntity } from '../entities';
+
 export type UserIdentificationProperties = 'username' | 'email' | 'uuid';
+
+export type FindUserQuery = FindQuery<UserEntity, UserIdentificationProperties>;
