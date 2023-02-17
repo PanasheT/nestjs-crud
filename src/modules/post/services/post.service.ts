8666dd1f18ad3021d3fe2888ac0f557e688435fa
PanasheT <ptafuma@proton.me>
2023-02-17 21:03:17 +0200

    feat: add find methods to post service

--- src/modules/post/services/post.service.ts
+++ src/modules/post/services/post.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserService } from 'src/modules/user/services';
 import { Repository } from 'typeorm';
 import { PostEntity } from '../entities';
 import { PostFactory } from '../factories';
@@ -9,6 +10,29 @@ export class PostService {
   constructor(
     @InjectRepository(PostEntity)
     private readonly repo: Repository<PostEntity>,
-    private readonly factory: PostFactory
+    private readonly factory: PostFactory,
+    private readonly userService: UserService
   ) {}
+
+  public async findOnePost(uuid: string): Promise<PostEntity> {
+    return await this.repo.findOneBy({ uuid, deleted: false });
+  }
+
+  public async findOnePostOrFail(uuid: string): Promise<PostEntity> {
+    try {
+      return await this.repo.findOneByOrFail({ uuid, deleted: false });
+    } catch {
+      throw new NotFoundException('Post not found');
+    }
+  }
+
+  public async findAllPosts(): Promise<PostEntity[]> {
+    return await this.repo.findBy({ deleted: false });
+  }
+
+  public async findAllUsersPosts(userUUID: string): Promise<PostEntity[]> {
+    await this.userService.findOneUserOrFail(userUUID, 'uuid');
+
+    return await this.repo.findBy({ user: { uuid: userUUID }, deleted: false });
+  }
 }
