787274f7ac29e175484ffd11d564bd90f032d558
PanasheT <ptafuma@proton.me>
2023-02-17 20:56:32 +0200

    feat: add create post to factory

--- src/modules/post/factories/post.factory.ts
+++ src/modules/post/factories/post.factory.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/modules/user/entities';
+import { UserService } from 'src/modules/user/services';
 import { Repository } from 'typeorm';
+import { CreatePostDto } from '../dtos';
 import { PostEntity } from '../entities';
 
 @Injectable()
 export class PostFactory {
   constructor(
     @InjectRepository(PostEntity)
-    private readonly repo: Repository<PostEntity>
+    private readonly repo: Repository<PostEntity>,
+    private readonly userService: UserService
   ) {}
+
+  public async createPost(model: CreatePostDto): Promise<PostEntity> {
+    const { userUUID, ...dto } = model;
+
+    const user: UserEntity = await this.userService.findOneUserOrFail(
+      userUUID,
+      'uuid'
+    );
+
+    return Object.assign(new PostEntity(), { user, ...dto });
+  }
 }
--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
 import { PostController } from './controllers';
 import { PostEntity } from './entities';
 import { PostFactory } from './factories';
 import { PostService } from './services';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [TypeOrmModule.forFeature([PostEntity]), UserModule],
   controllers: [PostController],
   exports: [PostService],
   providers: [PostService, PostFactory],
