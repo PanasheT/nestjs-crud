adec4f844f4594b47fadbd91908ca64ab89a053f
PanasheT <ptafuma@proton.me>
2023-02-18 16:34:38 +0200

    feat: user login endpoint

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserLoginDto, UserLoginResultDto } from '../dtos';
 import { AuthService } from '../services';
 
-@Controller('auths')
-@ApiTags('auths')
+@Controller('auth')
+@ApiTags('auth')
 export class AuthController {
-    constructor(private readonly service: AuthService) {}
+  constructor(private readonly service: AuthService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Login a user.' })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Login successful.',
+    type: UserLoginResultDto,
+  })
+  public async loginUser(
+    @Body() model: UserLoginDto
+  ): Promise<UserLoginResultDto> {
+    return await this.service.loginUser(model);
+  }
 }
