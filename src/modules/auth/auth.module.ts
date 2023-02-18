fd5ff7199dff9d402f957aade766d27993ddb20b
PanasheT <ptafuma@proton.me>
2023-02-18 15:27:48 +0200

    wip: init auth factory

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './controllers';
+import { AuthFactory } from './factories';
 import { AuthService } from './services';
 
 @Module({
@@ -20,6 +21,6 @@ import { AuthService } from './services';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthFactory],
 })
 export class AuthModule {}
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/modules/user/services';
+import { AuthFactory } from '../factories';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly factory: AuthFactory
+  ) {}
 }
