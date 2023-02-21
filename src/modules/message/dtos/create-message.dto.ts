43cbc47579ce84efa20d34db87ff96b17cdba2a6
PanasheT <ptafuma@proton.me>
2023-02-21 18:52:22 +0200

    feat: add create message dto

--- src/modules/message/dtos/create-message.dto.ts
+++ src/modules/message/dtos/create-message.dto.ts
@@ -1,1 +1,15 @@
-export class CreateMessageDto {}
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class CreateMessageDto {
+  @IsUUID()
+  @IsNotEmpty()
+  readonly sourceUUID: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  readonly conversationUUID: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly text: string;
+}
