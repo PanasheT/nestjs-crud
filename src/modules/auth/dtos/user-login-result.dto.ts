16c235858d9f1d50aef61b16567da6cdd06feaef
PanasheT <112612442+PanasheT@users.noreply.github.com>
2023-11-07 09:00:56 +0200

    refactor: add constructor to user login result

--- src/modules/auth/dtos/user-login-result.dto.ts
+++ src/modules/auth/dtos/user-login-result.dto.ts
@@ -3,4 +3,14 @@ import { UserDto } from 'src/modules/user/dtos/user.dto';
 export class UserLoginResultDto {
   readonly token: string;
   readonly user: UserDto;
+
+  public constructor(token: string, user: UserDto) {
+    if (token && user) {
+      this.token = token;
+      this.user = user;
+      return;
+    }
+
+    throw new Error('Failed to create login result.');
+  }
 }
--- src/modules/auth/factories/auth.factory.ts
+++ src/modules/auth/factories/auth.factory.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { UserDto, UserDtoFactory } from 'src/modules/user/dtos/user.dto';
 import { UserEntity } from 'src/modules/user/entities';
@@ -7,24 +6,14 @@ import { UserLoginResultDto } from '../dtos';
 
 @Injectable()
 export class AuthFactory {
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly config: ConfigService
-  ) {}
+  constructor(private readonly jwtService: JwtService) {}
 
   async generateSuccessfulLoginResult(
     model: UserEntity
   ): Promise<UserLoginResultDto> {
     const user: UserDto = UserDtoFactory(model);
+    const token: string = await this.jwtService.signAsync({ ...user });
 
-    const token: string = await this.jwtService.signAsync(
-      { ...user },
-      {
-        secret: this.config.get<string>('TOKEN_SECRET'),
-        expiresIn: this.config.get<number>('TOKEN_DURATION'),
-      }
-    );
-
-    return Object.assign(new UserLoginResultDto(), { user, token });
+    return new UserLoginResultDto(token, user);
   }
 }
